16b53d65426b6ab9baf02359dfd5d04293f0b0fd
Ilya <80617082+imatyushov@users.noreply.github.com>
2023-09-25 13:40:08 +0300

    feat: доделаны задачи по скроллу, остается все вынести в отдельный хук

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Scroll} from "./Scroll";
+import VirtualScroll from "./VirtualScroll";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
-  <React.StrictMode>
-     <Scroll/>
-  </React.StrictMode>
+      <VirtualScroll/>
 );
 
 // If you want to start measuring performance in your app, pass a function
